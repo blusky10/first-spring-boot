e0e60431369cd276e5d6eaa706864fc42fddbc7c
blusky10 <blusky0910@gmail.com>
2018-11-19 23:49:47 +0900

    Update: error test

--- spring-boot-test/src/test/java/com/polpid/springboottest/service/BookRestServiceTest.java
+++ spring-boot-test/src/test/java/com/polpid/springboottest/service/BookRestServiceTest.java
@@ -1,16 +1,20 @@
 package com.polpid.springboottest.service;
 
 import com.polpid.springboottest.domain.Book;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.HttpServerErrorException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 @RunWith(SpringRunner.class)
@@ -23,6 +27,9 @@ public class BookRestServiceTest {
     @Autowired
     private MockRestServiceServer server;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void rest_test(){
         this.server.expect(requestTo("/rest/test"))
@@ -30,5 +37,14 @@ public class BookRestServiceTest {
 
         Book book = this.bookRestService.getRestBook();
         assertThat(book.getTitle()).isEqualTo("test");
+    }
+
+    @Test
+    public void rest_error_test(){
+        this.server.expect(requestTo("/rest/test"))
+                .andRespond(withServerError());
+        this.thrown.expect(HttpServerErrorException.class);
+
+        this.bookRestService.getRestBook();
     }
 }
