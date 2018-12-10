750e92502750a8c03fcd724387c36d84b8716ccb
blusky10 <blusky0910@gmail.com>
2018-12-11 00:04:38 +0900

    Update: 스프링 부트 security + oaut2

--- spring-boot-web/src/main/java/com/polpid/springbootweb/SpringBootWebApplication.java
+++ spring-boot-web/src/main/java/com/polpid/springbootweb/SpringBootWebApplication.java
@@ -5,21 +5,33 @@ import com.polpid.springbootweb.domain.User;
 import com.polpid.springbootweb.domain.enums.BoardType;
 import com.polpid.springbootweb.repository.BoardRepository;
 import com.polpid.springbootweb.repository.UserRepository;
+import com.polpid.springbootweb.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication implements WebMvcConfigurer {
+
+    @Autowired
+    private UserArgumentResolver userArgumentResolver;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
+    }
 
     @Bean
     public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
@@ -32,14 +44,14 @@ public class SpringBootWebApplication {
                     .build());
 
             IntStream.rangeClosed(1, 200).forEach(index ->
-                        boardRepository.save(Board.builder()
-                                .title("게시글"+index)
-                                .subTitle("순서"+index)
-                                .content("컨텐츠")
-                                .boardType(BoardType.free)
-                                .createdDate(LocalDateTime.now())
-                                .updatedDate(LocalDateTime.now())
-                                .user(user).build())
+                    boardRepository.save(Board.builder()
+                            .title("게시글"+index)
+                            .subTitle("순서"+index)
+                            .content("컨텐츠")
+                            .boardType(BoardType.free)
+                            .createdDate(LocalDateTime.now())
+                            .updatedDate(LocalDateTime.now())
+                            .user(user).build())
             );
         };
     }
